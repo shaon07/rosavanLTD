fe2d27835c4a52c968fa191de77d3f73d2c42e0b
Shaon07 <alishaon77@gmail.com>
2022-07-27 10:48:04 +0600

    creating react app done

---
+++ src/Char.js
@@ -0,0 +1,52 @@
+import { Chart, registerables } from "chart.js";
+import React from "react";
+import { Line } from "react-chartjs-2";
+Chart.register(...registerables);
+
+const data = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "First dataset",
+      data: [500, 600, 530, 410, 440, 650, 560, 320, 670, 430, 640, 740],
+      fill: true,
+      backgroundColor: ["rgb(120,72,244,.20)"],
+      borderColor: "#7848F4",
+      lineTension: .5,
+    },
+  ],
+};
+
+var options = {
+  scales: {
+    y: {
+      min: 0,
+      max: 1000,
+      ticks: {
+        // forces step size to be 50 units
+        stepSize: 200,
+      },
+    },
+  },
+};
+
+export default function ChartShaon() {
+  return (
+    <div className="App" style={{ width: "600px" }}>
+      <Line data={data} options={options} />
+    </div>
+  );
+}
